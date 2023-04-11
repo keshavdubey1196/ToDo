25afc215a4f82717a4243c8abf26531ae4ab8f90
keshav <keshavsharma1196@gmail.com>
2023-04-12 01:31:29 +0530

    setting detail,create views

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -7,3 +10,16 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = "tasklist"
+    template_name = "todo/tasks.html"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "todo/task.html"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")

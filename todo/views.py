bb466fbea095e1f8fd9dd375d36381913b1e4c10
keshav <keshavsharma1196@gmail.com>
2023-04-13 01:30:48 +0530

    setting update,delete,login,logout views

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,20 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
+
+
+class CustomLoginView(LoginView):
+    template_name = "todo/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("tasks")
 
 
 class TaskList(ListView):
@@ -22,4 +32,16 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = "task"
     success_url = reverse_lazy("tasks")
